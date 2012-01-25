4e731e54b43d15412c9aafa66dbf0a924d2a94a5
Antar Iliev <summer.kenster@gmail.com>
2012-01-25 11:38:47 -0500

    Rewrote log reader for compatibility with new version.

--- LogReaderOnCrack.py
+++ LogReaderOnCrack.py
@@ -11,60 +11,66 @@ if len(sys.argv) > 2:
 	if sys.argv[2] == '-old':
 		oldWay = True
 
-if (len(log) < 5):
+sdiv = 6 #1.0.30 uses div, 1.0.53 uses span. It matters.
+ediv = 14
+if log[0][1:4] == 'div':
+	sdiv = 5
+	ediv = 7
+	
+if (len(log) < 20):
 	sys.exit()
 
-doubles = False
-#determine tier
-if log[2][0:25] == '<div class="TierSection">':
-	tier = log[2][string.find(log[2],"</b>")+4:len(log[2])-7]
-	if log[3][0:19] == '<div class="Rated">':
-		rated = log[3][string.find(log[3],"</b>")+4:len(log[3])-7]
-	else:
-		if log[5][0:19] == '<div class="Rated">':
-			rated = log[5][string.find(log[5],"</b>")+4:len(log[5])-7]
-		else:
-			print "Can't find the rating for "+filename
-			for line in range(0,15):
-				print log[line]
-			sys.exit()
-else:
-	if log[5][0:25] != '<div class="TierSection">':
-		print "Can't find the tier for "+filename
-		sys.exit()
-	tier = log[5][string.find(log[5],"</b>")+4:len(log[5])-7]
-	if log[6][0:19] == '<div class="Rated">':
-		rated = log[6][string.find(log[6],"</b>")+4:len(log[6])-7]
-	else:
-		if log[8][0:19] == '<div class="Rated">':
-			rated = log[8][string.find(log[8],"</b>")+4:len(log[8])-7]
-		else:
-			print "Can't find the rating for "+filename
-			for line in range(0,15):
-				print log[line]
-			sys.exit()
+#get rid of all the debug lines--it would be awesome to use them, though
+for line in range(len(log)-1,-1,-1):
+	if log[line][0:4] == '<!--':
+		del log[line]
 
-#if tier != 'Standard OU' or rated != 'Rated':
-#	sys.exit()
-if tier in ['VGC 2009', 'VGC 2010', 'VGC 2011', 'VGC 2012']:
-	doubles = True
-#make sure the battle lasted at least six turns (to discard early forfeits)
+doubles = False
+CC = False
 longEnough = False
+#determine tier
+tierFound=False
+for line in range(2,11):
+	if log[line][sdiv:sdiv+11] == 'class="Tier':
+		tierFound=True
+		tier = log[line][string.find(log[line],"</b>")+4:len(log[line])-ediv]
+		ratingFound=False
+		for ln in range(line+1,line+10):
+			if log[ln][sdiv:sdiv+14] == 'class="Rated">':
+				ratingFound=True
+				rated = log[ln][string.find(log[ln],"</b>")+4:len(log[ln])-ediv]
+				break
+		if ratingFound==False:
+			print "Can't find the rating for "+filename
+			for line in range(0,20):
+				print log[line]
+			sys.exit()
+		break
+if tierFound == False:
+	print "Can't find the tier for "+filename
+	sys.exit()
+if CC == True and tier != "Challenge Cup":
+	sys.exit()
+
+#check for log length, doubles/triples and Challenge Cup
 for line in log:
-	if line == '<div class="BeginTurn"><b><span style=\'color:#0000ff\'>Start of turn 6</span></b></div>\n':
+	if line[sdiv:len(line)-ediv] in ['class="BeginTurn"><b><span style=\'color:#0000ff\'>Start of turn 6</span></b>','class="BeginTurn"><b><span style=\'color:#0000ff\'>Start of turn 6!</span></b>']:
 		longEnough = True
 		break
-		
-	
+	elif line[sdiv:len(line)-ediv] in ['class="Mode"><b><span style=\'color:#0000ff\'>Mode: </span></b>Doubles','class="Mode"><b><span style=\'color:#0000ff\'>Mode: </span></b>Triples']:
+		doubles = True
+	elif line[sdiv:len(line)-ediv] == 'class="Clause"><b><span style=\'color:#0000ff\'>Rule: </span></b>Challenge Cup':
+		CC = True
+
 #get info on the trainers & pokes involved
 ts = []
 skip = 0
 if oldWay == False:
 	for line in range(1,len(log)):
-		if log[line][0:19] == '<div class="Teams">':
+		if log[line][sdiv:sdiv+14] == 'class="Teams">':
 			for x in range(0,2):
-				trainer = log[line+x][50:string.rfind(log[line+x],"'s team:")]
-				if string.find(trainer,"send out") > -1:
+				trainer = log[line+x][sdiv+45:string.rfind(log[line+x],"'s team:")]
+				if string.find(trainer,"sent out") > -1:
 					print trainer+" is a dick."
 					sys.exit()
 				stemp = ""
@@ -82,8 +88,8 @@ if oldWay == False:
 				ts.append([trainer,stemp])
 			break
 
-if rated == 'Rated':
-	forfeitLog.write(tier+"\t"+ts[0][0]+"\t"+ts[11][0])
+if rated == 'Rated' and oldWay == False:
+	forfeitLog.write(tier+"\t"+ts[0][0]+"\t"+ts[1][0])
 	if (longEnough == False) and (doubles == False):
 		forfeitLog.write("\t*\n")
 		forfeitLog.close()
@@ -94,13 +100,13 @@ forfeitLog.close()
 if (line == len(log)) or oldWay == True: #it's an old log, so find pokes the old way
 	#find all "sent out" messages
 	for line in range(5,len(log)):
-		if log[line][0:21] == '<div class="SendOut">':
-			ttemp = log[line][21:string.find(log[line],' sent out ')]
+		if log[line][sdiv:sdiv+16] == 'class="SendOut">':
+			ttemp = log[line][sdiv+16:string.find(log[line],' sent out ')]
 			#determine whether the pokemon is nicknamed or not
-			if log[line][len(log[line])-8] == ')':
-				stemp = log[line][string.rfind(log[line],'(')+1:len(log[line])-8]
+			if log[line][len(log[line])-ediv-1] == ')':
+				stemp = log[line][string.rfind(log[line],'(')+1:len(log[line])-ediv-1]
 			else:
-				stemp = log[line][string.rfind(log[line],'sent out ')+9:len(log[line])-8]
+				stemp = log[line][string.rfind(log[line],'sent out ')+9:len(log[line])-ediv-1]
 
 			#determine whether this entry is already in the list
 			match = 0
@@ -128,19 +134,19 @@ if (line == len(log)) or oldWay == True: #it's an old log, so find pokes the old
 active = [-1,-1]
 t=0
 for line in range(1,len(log)):
-	if log[line][0:21] == '<div class="SendOut">':
+	if log[line][sdiv:sdiv+16] == 'class="SendOut">':
 		for x in range(0,2):
 			#ID trainer
-			trainer = log[line+x][21:string.find(log[line+x],' sent out ')]
+			trainer = log[line+x][sdiv+16:string.find(log[line+x],' sent out ')]
 			if trainer == ts[0][0]:
 				t=0
 			else:
 				t=1
 			#it matters whether the poke is nicknamed or not
-			if log[line+x][len(log[line+x])-8] == ')':
-				species = log[line+x][string.rfind(log[line+x],'(')+1:len(log[line+x])-8]
+			if log[line+x][len(log[line+x])-ediv-1] == ')':
+				species = log[line+x][string.rfind(log[line+x],'(')+1:len(log[line+x])-ediv-1]
 			else:
-				species = log[line+x][string.rfind(log[line+x],'sent out ')+9:len(log[line+x])-8]
+				species = log[line+x][string.rfind(log[line+x],'sent out ')+9:len(log[line+x])-ediv-1]
 			for i in range(0,6):
 				if species == ts[6*t+i][1]:
 					active[t] = i
@@ -174,7 +180,7 @@ else:
 
 	for line in range(start,len(log)):
 		#identify what kind of message is on this line
-		linetype = log[line][12:string.find(log[line],'">')]
+		linetype = log[line][sdiv+7:string.find(log[line],'">')]
 
 		if linetype == "BeginTurn":
 			#reset for start of turn
@@ -187,13 +193,11 @@ else:
 
 		if linetype == "UseAttack": #check for Roar, etc.; U-Turn, etc.
 			#identify move
-			move = log[line][string.rfind(log[line],"'>")+2:len(log[line])-19]
+			move = log[line][string.rfind(log[line],"'>")+2:len(log[line])-ediv-12]
 			if move in ["Roar","Whirlwind","Circle Throw","Dragon Tail"]:
 				roar = 1
-			elif move in ["U-Turn","Volt Switch","Baton Pass"]:
-				if line+3 < len(log):
-					if log[line][12:string.find(log[line],'">')] == "SendBack":
-						uturn = 1
+			elif move in ["U-Turn","U-turn","Volt Switch","Baton Pass"]:
+				uturn = 1
 
 		elif linetype == "ItemMessage": #check for Red Card, Eject Button
 			#search for relevant items
@@ -204,14 +208,13 @@ else:
 
 		elif linetype == "Ko": #KO
 			ko = ko+1
-		
 			#make sure it's not the end of the battle
 			o = p = 0
 			if line+2 < len(log):
 				o = 1
 			if line+1 < len(log):
 				p = 1
-			if log[line+2*o][12:string.find(log[line+2*o],'">')] == "BattleEnd":
+			if log[line+2*o][sdiv+7:string.find(log[line+2*o],'">')] == "BattleEnd":
 				pokes = [ts[active[0]][1],ts[active[1]+6][1]]
 				matchup=pokes[0]+' vs. '+pokes[1]+': '
 				if ko == 1:
@@ -224,13 +227,15 @@ else:
 				else:
 					matchup = matchup + "no clue what happened"
 				matchups.append(matchup)
-			elif log[line+p][12:string.find(log[line+p],'">')] == "SendBack":
+			elif uturn == 1:
+				uturn=0
 				uturnko=1
+
 		elif linetype == "SendBack": #switch out
 			switch = 1
 		elif linetype == "SendOut":
 			#ID trainer
-			trainer = log[line][21:string.find(log[line],' sent out ')]
+			trainer = log[line][sdiv+16:string.find(log[line],' sent out ')]
 			if trainer == ts[0][0]:
 				t=0
 			else:
@@ -242,7 +247,7 @@ else:
 				o = 1
 			if ignore == 1:
 				ignore = 0
-			elif (o == 1) and (log[line+2*o][12:string.find(log[line+2*o],'">')] == "SendBack"):
+			elif (o == 1) and (log[line+2*o][sdiv+7:string.find(log[line+2*o],'">')] == "SendBack"):
 				doubleSwitch = active[t]+t*6
 			else:
 				#close out old matchup
@@ -256,7 +261,7 @@ else:
 				if doubleSwitch > -1:
 					matchup = matchup + "double switch"
 				elif (uturnko == 1):
-					KOs[active[(t+1)%2]+6*((t+1)%2)]=KOs[active[(t+1)%2]+6*((t+1)%2)]+1
+					KOs[active[(t)%2]+6*((t)%2)]=KOs[active[(t)%2]+6*((t)%2)]+1
 					matchup = matchup + ts[active[(t+1)%2]+((t+1)%2)*6][1] + " was u-turn KOed"
 					ignore = 1
 				elif ko == 1:
@@ -278,10 +283,10 @@ else:
 			#new matchup!
 			uturn = roar = 0
 			#it matters whether the poke is nicknamed or not
-			if log[line][len(log[line])-8] == ')':
-				species = log[line][string.rfind(log[line],'(')+1:len(log[line])-8]
+			if log[line][len(log[line])-ediv-1] == ')':
+				species = log[line][string.rfind(log[line],'(')+1:len(log[line])-ediv-1]
 			else:
-				species = log[line][string.rfind(log[line],'sent out ')+9:len(log[line])-8]
+				species = log[line][string.rfind(log[line],'sent out ')+9:len(log[line])-ediv-1]
 			for i in range(0,6):
 				if species == ts[6*t+i][1]:
 					active[t] = i
