c1e96382a31cdb11e46071984f7e194f7c061767
Antar Iliev <summer.kenster@gmail.com>
2012-03-14 13:34:32 -0400

    PU

--- TierUpdate.py
+++ TierUpdate.py
@@ -6,9 +6,16 @@ file = open("pokemons.txt")
 pokelist = file.readlines()
 file.close()
 
+file = open("NFE.txt")
+NFElist = file.readlines()
+file.close()
+
 lsname = []
 for line in range(0,len(pokelist)):
 	lsname.append(pokelist[line][str.find(pokelist[line],' ')+1:len(pokelist[line])-1])
+NFE = []
+for line in range(0,len(NFElist)):
+	NFE.append(NFElist[line][str.find(NFElist[line],' ')+1:len(NFElist[line])-1])
 file = open("tiers.yml")
 tiersYML = file.readlines()
 file.close()
@@ -18,7 +25,7 @@ BLYML = file.readlines()
 file.close()
 
 curTiers = ['NU' for i in range(len(pokelist))] #any pokemon not found in the list must be NU
-usage = [[0,0,0] for i in range(len(pokelist))] #track usage across all three tiers
+usage = [[0,0,0,0] for i in range(len(pokelist))] #track usage across all three tiers
 
 #there's a lot of unrelated stuff in tiers.yml
 for i in range(len(tiersYML)):
@@ -282,6 +289,41 @@ for i in range(6,len(table)):
 for i in range(0,len(tempUsage)):
 	usage[i][2] = usage[i][2]+6.0*tempUsage[i]/sum(tempUsage)/24
 
+#...first month's NU
+filename = str(sys.argv[1])+"/Stats/Standard NU Rated.txt"
+file = open(filename)
+table=file.readlines()
+file.close()
+
+tempUsage = [0 for i in range(len(pokelist))] #really dumb that I have to do this
+
+for i in range(6,len(table)):
+	name = table[i][10:26]
+	
+	if (name[0] == '-'):
+		break
+
+	while name[len(name)-1] == ' ': 
+		#remove extraneous spaces
+		name = name[0:len(name)-1]
+	
+	count = table[i][28:35]
+	while count[len(count)-1] == ' ':
+		#remove extraneous spaces
+		count = count[0:len(count)-1]
+	found = False
+	for j in range(0,len(lsname)):
+		if name == lsname[j]:
+			tempUsage[j]=float(count)
+			found = True
+			break
+	if not found:
+		print name+" not found!"
+		sys.exit() 
+
+for i in range(0,len(tempUsage)):
+	usage[i][3] = usage[i][3]+6.0*tempUsage[i]/sum(tempUsage)/24
+
 
 #...second month's OU
 filename = str(sys.argv[2])+"/Stats/Standard OU Rated.txt"
@@ -388,6 +430,40 @@ for i in range(6,len(table)):
 for i in range(0,len(tempUsage)):
 	usage[i][2] = usage[i][2]+3.0*6.0*tempUsage[i]/sum(tempUsage)/24
 
+#...second month's NU
+filename = str(sys.argv[2])+"/Stats/Standard NU Rated.txt"
+file = open(filename)
+table=file.readlines()
+file.close()
+
+tempUsage = [0 for i in range(len(pokelist))] #really dumb that I have to do this
+
+for i in range(6,len(table)):
+	name = table[i][10:26]
+	
+	if (name[0] == '-'):
+		break
+
+	while name[len(name)-1] == ' ': 
+		#remove extraneous spaces
+		name = name[0:len(name)-1]
+	
+	count = table[i][28:35]
+	while count[len(count)-1] == ' ':
+		#remove extraneous spaces
+		count = count[0:len(count)-1]
+	found = False
+	for j in range(0,len(lsname)):
+		if name == lsname[j]:
+			tempUsage[j]=float(count)
+			found = True
+			break
+	if not found:
+		print name+" not found!"
+		sys.exit() 
+
+for i in range(0,len(tempUsage)):
+	usage[i][3] = usage[i][3]+3.0*6.0*tempUsage[i]/sum(tempUsage)/24
 
 
 #...third month's OU
@@ -495,12 +571,48 @@ for i in range(6,len(table)):
 for i in range(0,len(tempUsage)):
 	usage[i][2] = usage[i][2]+20*6.0*tempUsage[i]/sum(tempUsage)/24
 
+#...third month's NU
+filename = str(sys.argv[3])+"/Stats/Standard NU Rated.txt"
+file = open(filename)
+table=file.readlines()
+file.close()
+
+tempUsage = [0 for i in range(len(pokelist))] #really dumb that I have to do this
+
+for i in range(6,len(table)):
+	name = table[i][10:26]
+	
+	if (name[0] == '-'):
+		break
+
+	while name[len(name)-1] == ' ': 
+		#remove extraneous spaces
+		name = name[0:len(name)-1]
+	
+	count = table[i][28:35]
+	while count[len(count)-1] == ' ':
+		#remove extraneous spaces
+		count = count[0:len(count)-1]
+	found = False
+	for j in range(0,len(lsname)):
+		if name == lsname[j]:
+			tempUsage[j]=float(count)
+			found = True
+			break
+	if not found:
+		print name+" not found!"
+		sys.exit() 
+
+for i in range(0,len(tempUsage)):
+	usage[i][3] = usage[i][3]+20*6.0*tempUsage[i]/sum(tempUsage)/24
+
 
 #generate three-month tables and start working on that new tier list
-newTiers = ['NU' for i in range(len(pokelist))]
+newTiers = ['PU' for i in range(len(pokelist))]
 OU = []
 UU = []
 RU = []
+NU = []
 for i in range(0,len(usage)):
 	if usage[i][0] > 0.0:
 		OU.append([i,usage[i][0]])
@@ -508,9 +620,12 @@ for i in range(0,len(usage)):
 		UU.append([i,usage[i][1]])
 	if usage[i][2] > 0.0:
 		RU.append([i,usage[i][2]])
+	if usage[i][3] > 0.0:
+		NU.append([i,usage[i][3]])
 OU = sorted(OU, key=lambda OU:-OU[1])
 UU = sorted(UU, key=lambda UU:-UU[1])
 RU = sorted(RU, key=lambda RU:-RU[1])
+NU = sorted(NU, key=lambda NU:-NU[1])
 
 print "[HIDE=OU][CODE]"
 print "Three-month usage for Standard OU"
@@ -526,7 +641,7 @@ for i in range(1,len(OU)):
 print '[/B] | %-4d | %-15s | %6.3f%% |' % (i+1,lsname[OU[i][0]],100.0*OU[i][1])
 for i in range(0,start):
 	newTiers[OU[i][0]] = 'OU'
-for i in range(start,len(OU)):
+for i in range(start+1,len(OU)):
 	print ' | %-4d | %-15s | %6.3f%% |' % (i+1,lsname[OU[i][0]],100.0*OU[i][1])
 print " + ---- + --------------- + ------- +[/CODE][/HIDE]"
 
@@ -544,9 +659,9 @@ for i in range(1,len(UU)):
 	print ' | %-4d | %-15s | %6.3f%% |' % (i+1,lsname[UU[i][0]],100.0*UU[i][1])
 print '[/B] | %-4d | %-15s | %6.3f%% |' % (i+1,lsname[UU[i][0]],100.0*UU[i][1])
 for i in range(0,start):
-	if (newTiers[UU[i][0]] == 'NU'):
+	if (newTiers[UU[i][0]] == 'PU'):
 		newTiers[UU[i][0]] = 'UU'
-for i in range(start,len(UU)):
+for i in range(start+1,len(UU)):
 	print ' | %-4d | %-15s | %6.3f%% |' % (i+1,lsname[UU[i][0]],100.0*UU[i][1])
 print " + ---- + --------------- + ------- +[/CODE][/HIDE]"
 print ""
@@ -565,10 +680,31 @@ for i in range(1,len(RU)):
 	print ' | %-4d | %-15s | %6.3f%% |' % (i+1,lsname[RU[i][0]],100.0*RU[i][1])
 print '[/B] | %-4d | %-15s | %6.3f%% |' % (i+1,lsname[RU[i][0]],100.0*RU[i][1])
 for i in range(0,start):
-	if (newTiers[RU[i][0]] == 'NU'):
+	if (newTiers[RU[i][0]] == 'PU'):
 		newTiers[RU[i][0]] = 'RU'
-for i in range(start,len(RU)):
+for i in range(start+1,len(RU)):
 	print ' | %-4d | %-15s | %6.3f%% |' % (i+1,lsname[RU[i][0]],100.0*RU[i][1])
+print " + ---- + --------------- + ------- +[/CODE][/HIDE]"
+print ""
+
+print ""
+print "[HIDE=NU][CODE]"
+print "Three-month usage for Standard NU"
+print " + ---- + --------------- + ------- + "
+print " | Rank | Pokemon         | Percent | "
+print " + ---- + --------------- + ------- + "
+print ' [B]| %-4d | %-15s | %6.3f%% |' % (1,lsname[NU[0][0]],NU[0][1]*100)
+for i in range(1,len(NU)):
+	if NU[i][1] < 0.0340636711:
+		start = i
+		break
+	print ' | %-4d | %-15s | %6.3f%% |' % (i+1,lsname[NU[i][0]],100.0*NU[i][1])
+print '[/B] | %-4d | %-15s | %6.3f%% |' % (i+1,lsname[NU[i][0]],100.0*NU[i][1])
+for i in range(0,start):
+	if (newTiers[NU[i][0]] == 'PU'):
+		newTiers[NU[i][0]] = 'NU'
+for i in range(start+1,len(NU)):
+	print ' | %-4d | %-15s | %6.3f%% |' % (i+1,lsname[NU[i][0]],100.0*NU[i][1])
 print " + ---- + --------------- + ------- +[/CODE][/HIDE]"
 print ""
 
@@ -585,15 +721,18 @@ for i in range(len(curTiers)):
 		if newTiers[i] not in ['OU','UU']:
 			newTiers[i] = 'BL2'
 	elif curTiers[i] == 'BL3':
-		if newTiers[i] == 'NU':
+		if newTiers[i] in ['NU', 'PU']:
 			newTiers[i] = 'BL3'
 	#now to prevent multi-tier drops
 	elif curTiers[i] == 'OU':
 		if newTiers[i] in ['RU','NU']:
 			newTiers[i] = 'UU'
 	elif curTiers[i] == 'UU':
-		if newTiers[i] == 'NU':
+		if newTiers[i] in ['NU','PU']:
 			newTiers[i] = 'RU'
+	elif curTiers[i] == 'RU':
+		if newTiers[i] == 'PU':
+			newTiers[i] = 'NU'
 
 	#stupid formes are stupid
 	if i == 173:
@@ -620,6 +759,8 @@ for i in range(len(curTiers)):
 		newTiers[i] = newTiers[656] #sawsbuck
 	elif i == 722:
 		newTiers[i] = newTiers[721] #meloetta
+	elif i > 723:
+		newTiers[i] = newTiers[723] #genesect
 
 	#replace names with numbers (really should have used numbers from the beginning) so it's sortable
 	if newTiers[i] == 'Unreleased':
@@ -640,6 +781,11 @@ for i in range(len(curTiers)):
 		poke.append([i,3.5])
 	elif newTiers[i] == 'NU':
 		poke.append([i,4.0])
+	elif newTiers[i] == 'PU':
+		if lsname[i] not in NFE:
+			poke.append([i,5.0])
+		else:
+			poke.append([i,5.1])
 
 #print tier list
 poke = sorted(poke, key=lambda poke:poke[1])
@@ -648,8 +794,6 @@ print "[B]Unreleased[/B]"
 print "[CODE]"
 print lsname[poke[0][0]]
 for i in range(1,len(poke)):
-	if poke[i][0] in [173]+range(507,534)+range(551,559)+[616,622,653,654,655,657,658,659,722]:
-		continue
 	if poke[i][1] != poke[i-1][1]:
 		print "[/CODE]"
 		print ""
@@ -669,11 +813,19 @@ for i in range(1,len(poke)):
 			print "[B]BL3[/B]"
 		elif poke[i][1] == 4.0:
 			print "[B]NU[/B]"
+		elif poke[i][1] == 5.0:
+			print "[B]PU[/B]"
+			print 'Note that "PU" is not a real tier and will NOT be supported on the simulator. I post the list here just as a point of reference--these pokemon are the NU of NU.'
 			print "[HIDE]"
+		elif poke[i][1] == 5.1:
+			print "NFEs:"	
 		print "[CODE]"
+	if poke[i][0] in [0,173]+range(507,534)+range(551,559)+[616,622,653,654,655,657,658,659,722,724,725,726,727]:
+		continue
 	printme=lsname[poke[i][0]]
 	if newTiers[poke[i][0]] != curTiers[poke[i][0]]:
-		printme="[B]"+printme
+		if newTiers[poke[i][0]] != 'PU':
+			printme="[B]"+printme
 		if newTiers[poke[i][0]] == 'OU':
 			printme=printme+" up"
 		elif newTiers[poke[i][0]] == 'UU':
@@ -686,9 +838,10 @@ for i in range(1,len(poke)):
 				printme=printme+" up"
 			else:
 				printme=printme+" down"
-		else:
+		elif newTiers[poke[i][0]] == 'NU':
 			printme=printme+" down"
-		printme=printme+" from "+curTiers[poke[i][0]]+"[/B]"
+		if newTiers[poke[i][0]] != 'PU':
+			printme=printme+" from "+curTiers[poke[i][0]]+"[/B]"
 	print printme
 
 print "[/CODE][/HIDE]"
