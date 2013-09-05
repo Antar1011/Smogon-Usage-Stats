6da15548413c9602ec00550c08bcf4195e7297a1
Antar1011 <summer.kenster@gmail.com>
2013-09-05 07:49:53 -0700

    Wrote a script for testing various ladder rating systems. Starting with Zarel and Antar's Modified Elo

---
+++ ZAME.py
@@ -0,0 +1,26 @@
+from common import victoryChance
+
+K=50
+
+def newPlayer():
+	return 1000
+
+def update(ratings,outcome):
+	S={}
+	if outcome == 1:
+		S['p1']=1
+	elif outcome == 2:
+		S['p1']=0
+	else: #if outcome == 0
+		S['p1']=0.5
+	S['p2']=1.0-S['p1']
+
+	E={}
+	E['p1']=victoryChance(ratings['p1']['r'],ratings['p1']['rd'],ratings['p2']['r'],ratings['p2']['rd'])
+	E['p2']=1.0-E['p1']
+	
+	for p in ['p1','p2']:
+		ratings[p]['ladderRating']=ratings[p]['ladderRating']+K*(S[p]-E[p])
+
+def getSortable(ladderRating):
+	return ladderRating
---
+++ ladderRatingTester.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
+import sys
+import gzip
+import json
+import math
+import cPickle as pickle
+import ZAME as ladderRatingSystem
+
+ladder={}
+metrics=set(['r','rd','rpr','rprd'])
+trajectories={}
+trajectoriesSaveFile=False
+
+idx=1
+while idx<len(sys.argv):
+	filename=sys.argv[idx]
+	if filename == '-t':
+		trajectoriesSaveFile=sys.argv[idx+1]
+		idx=idx+2
+		continue
+		
+	file = gzip.open(filename,'rb')
+	raw = file.read()
+	file.close()
+
+	raw=raw.split('][')
+	for i in range(len(raw)):
+		if (i>0):
+			raw[i]='['+raw[i]
+		if (i<len(raw)-1):
+			raw[i]=raw[i]+']'
+
+	for line in raw:
+		battles = json.loads(line)
+		for battle in battles:
+			ratings={}
+			winner=0
+			ladderError = False
+			for player in ['p1','p2']:
+				if 'rating' in battle[player].keys():
+					if metrics.issubset(battle[player]['rating'].keys()):
+						if battle[player]['rating']['rd'] != 0.0 and battle[player]['rating']['rprd'] != 0.0:
+							ratings[player]={}
+							for metric in metrics:
+								ratings[player][metric]=battle[player]['rating'][metric]
+				if player not in ratings.keys(): #in the event of a ladder error
+					ladderError = True
+					ratings[player]={}
+					ratings[player]['r']=ratings[player]['rpr']=1500.0
+					ratings[player]['rd']=ratings[player]['rprd']=350.0
+				if battle[player]['trainer'] in ladder.keys():
+					ratings[player]['ladderRating']=ladder[battle[player]['trainer']]['ladderRating']
+				else:
+					ratings[player]['ladderRating']=ladderRatingSystem.newPlayer()
+					ladder[battle[player]['trainer']]={}
+					ladder[battle[player]['trainer']]['r']=ladder[battle[player]['trainer']]['rpr']=1500.0
+					ladder[battle[player]['trainer']]['rd']=ladder[battle[player]['trainer']]['rprd']=350.0
+					trajectories[battle[player]['trainer']]=[]
+
+			if 'outcome' in battle['p1'].keys():
+				if battle['p1']['outcome'] == 'win':
+					winner=1
+				elif battle['p1']['outcome'] == 'loss':
+					winner=2		
+			ladderRatingSystem.update(ratings,winner)
+
+			for player in ['p1','p2']:
+				opp={'p1':'p2','p2':'p1'}
+				if not ladderError:
+					ladder[battle[player]['trainer']]=ratings[player]
+				else:	
+					ladder[battle[player]['trainer']]['ladderRating']=ratings[player]['ladderRating']
+				trajectories[battle[player]['trainer']].append({'score':ratings[player]['ladderRating'],'opponent':ratings[opp[player]]})
+				if 'outcome' in battle[player].keys():
+					trajectories[battle[player]['trainer']][-1]['outcome']=battle[player]['outcome']
+				else:
+					trajectories[battle[player]['trainer']][-1]['outcome']='tie'
+	idx=idx+1
+
+if trajectoriesSaveFile:			
+	pickle.dump(trajectories,open(trajectoriesSaveFile,'w'))
+
+sys.exit(0)
+print 'Username,r,rd,rpr,rprd,acre,gxe,ladderScore'
+for player in ladder.keys():
+	r=ladder[player]['r']
+	rd=ladder[player]['rd']
+	rpr=ladder[player]['rpr']
+	rprd=ladder[player]['rprd']
+	gxe = round(10000 / (1 + pow(10.0,(((1500 - rpr)) * math.pi / math.sqrt(3 * pow(math.log(10.0),2.0) * pow(rprd,2.0) + 2500 * (64 * pow(math.pi,2.0) + 147 * pow(math.log(10.0),2))))))) / 100
+	acre= rpr-1.4079126393*rprd
+	ladderScore=ladderRatingSystem.getSortable(ladder[player]['ladderRating'])
+
+	print player+','+str(r)+','+str(rd)+','+str(rpr)+','+str(rprd)+','+str(acre)+','+str(gxe)+','+str(ladderScore)	
