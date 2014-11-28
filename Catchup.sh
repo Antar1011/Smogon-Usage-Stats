2b96a7d6bfbd565a24071be6d2147527f0920c2f
Antar1011 <summer.kenster@gmail.com>
2014-11-28 09:00:07 -0500

    Added bash scripts (GNU Parallel is SEXY)

---
+++ Catchup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#This file is included solely to be used as an example. It will likely need to be heavily modified from month to month
+#(or from run to run)
+
+logFolder=/home/ps/showdown/logs
+month="2014-11"
+
+#rm -r Raw
+#mkdir Raw
+
+#for day in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+for day in 27
+do
+	for i in $logFolder/$month/*
+	do
+		tier=$(basename $i)
+		if [[ $tier != *ackmons* ]] && [ -d $logFolder/$month/$tier/$month-$day ]; then
+			echo Processing $tier/$month-$day
+			pypy batchLogReader.py $logFolder/$month/$tier/$month-$day/ $tier
+		fi
+	done
+done
+echo $(date)
+./MonthlyAnalysis.sh
+echo $(date)
