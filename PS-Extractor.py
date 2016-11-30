70a9da43a59a75a456a17a2d34878e87c388ac20
Antar1011 <summer.kenster@gmail.com>
2016-11-30 00:52:57 -0500

    Update data files for Gen VII

--- PS-Extractor.py
+++ PS-Extractor.py
@@ -1,64 +1,35 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-#The goal of this script is to take a json version of the "exports" object used on Pokemon Showdown
-#and pull out the data that's needed for the scripts.
+"""The goal of this script is to take a json version of the "exports" object
+used on Pokemon Showdown and pull out the data that's needed for the scripts."""
 
 import json
 import cPickle as pickle
+from onix import contexts
 
-exports=json.loads(open('exports.json').readline())
+ctx = contexts.get_standard_context(force_refresh=True)
 
-keyLookup={}
-baseStats={}
-types={}
+keyLookup = {}
+baseStats = {}
+types = {}
 
-for key in exports['BattleItems']:
-	item = exports['BattleItems'][key]
-	keyLookup[item['id']]=item['name']
+for item in ctx.items.values():
+    keyLookup[item['id']] = item['name']
 
-for key in exports['BattleMovedex']:
-	move = exports['BattleMovedex'][key]
-	keyLookup[move['id']]=move['name']
+for move in ctx.moves.values():
+    keyLookup[move['id']] = move['name']
 
-for key in exports['BattleAbilities']:
-	ability = exports['BattleAbilities'][key]
-	keyLookup[ability['id']]=ability['name']
+for ability in ctx.abilities.values():
+    keyLookup[ability['id']] = ability['name']
 
-for key in exports['BattlePokedex']:
-	poke = exports['BattlePokedex'][key]
-	baseStats[key]=poke['baseStats']
-	keyLookup[key]=poke['species']
-	types[key]=poke['types']
+for k, v in ctx.pokedex.items():
+    baseStats[k] = v['baseStats']
+    keyLookup[k] = v['species']
+    types[k] = v['types']
 
-nmod = {'hardy': [10,10,10,10,10],
-	'lonely': [11,9,10,10,10],
-	'brave': [11,10,9,10,10],
-	'adamant': [11,10,10,9,10],
-	'naughty': [11,10,10,10,9],
-	'bold': [9,11,10,10,10],
-	'docile': [10,10,10,10,10],
-	'relaxed': [10,11,9,10,10],
-	'impish': [10,11,10,9,10],
-	'lax': [10,11,10,10,9],
-	'timid': [9,10,11,10,10],
-	'hasty': [10,9,11,10,10],
-	'serious': [10,10,10,10,10],
-	'jolly': [10,10,11,9,10],
-	'naive': [10,10,11,10,9],
-	'modest': [9,10,10,11,10],
-	'mild': [10,9,10,11,10],
-	'quiet': [10,10,9,11,10],
-	'bashful': [10,10,10,10,10],
-	'rash': [10,10,10,11,9],
-	'calm': [9,10,10,10,11],
-	'gentle': [10,9,10,10,11],
-	'sassy': [10,10,9,10,11],
-	'careful': [10,10,10,9,11],
-	'quirky': [10,10,10,10,10]}
+for k, v in ctx.natures.items():
+    keyLookup[k] = v['name']
 
-for nature in nmod:
-	keyLookup[nature] = nature.title()
-
-json.dump(baseStats,open('baseStats.json','w+'))
-json.dump(types,open('types.json','w+'))
-pickle.dump(keyLookup,open('keylookup.pickle','w+'))
+json.dump(baseStats, open('baseStats.json', 'w+'))
+json.dump(types, open('types.json', 'w+'))
+pickle.dump(keyLookup, open('keylookup.pickle', 'w+'))
